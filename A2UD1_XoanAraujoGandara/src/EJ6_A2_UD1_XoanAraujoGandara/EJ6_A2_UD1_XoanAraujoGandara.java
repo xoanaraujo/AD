422ab01510fee35c629f5315b4ef1b50afd5a1be
xoanaraujo <115078254+xoanaraujo@users.noreply.github.com>
2023-09-19 12:53:29 +0200

    ...

--- A2UD1_XoanAraujoGandara/src/EJ6_A2_UD1_XoanAraujoGandara/EJ6_A2_UD1_XoanAraujoGandara.java
+++ A2UD1_XoanAraujoGandara/src/EJ6_A2_UD1_XoanAraujoGandara/EJ6_A2_UD1_XoanAraujoGandara.java
@@ -8,6 +8,7 @@ import EJ3_A2_UD1_xoanaraujogandara.*;
 import EJ2_A2_UD1_xoanaraujogandara.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.PrintStream;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
@@ -17,15 +18,15 @@ import java.util.Scanner;
  *
  * @author usuario
  */
-public class EJ3_A2_UD1_XoanAraujoGandara {
+public class EJ6_A2_UD1_XoanAraujoGandara {
 
     private static Scanner sc = new Scanner(System.in);
     private static PrintStream ps = new PrintStream(System.out);
-    private static String dirPath;
 
     public static void main(String[] args) {
-        dirPath = sc.nextLine();
-        File dir = new File(dirPath);
+        File dir = new File(sc.nextLine());
+        System.out.println("El archivo contiene: ");
+        Filtro filtro = new Filtro("." + sc.nextLine());
         if (!dir.exists()) {
             try {
                 throw new FileNotFoundException("No existe el fichero");
@@ -40,13 +41,14 @@ public class EJ3_A2_UD1_XoanAraujoGandara {
             }
         } else {
             ps.println("--LISTANDO EL DIRECTORIO " + dir + " --");
-            getContent(dir, 0);
+            getContent(dir, 0, filtro);
         }
 
     }
 
-    private static void getContent(File dir, int nParents) {
-        for (File file : dir.listFiles()) {
+    private static void getContent(File dir, int nParents, Filtro filtro) {
+        File[] files = dir.listFiles(filtro);
+        for (File file : files) {
             StringBuilder strB = new StringBuilder("");
             for (int i = 0; i < nParents + 1; i++) {
                 strB.append("---");
@@ -55,7 +57,7 @@ public class EJ3_A2_UD1_XoanAraujoGandara {
             if (file.isDirectory()) {
                 ps.print("| " + file.getName() + "<DIR>\n");
                 if(file.listFiles() != null){
-                    getContent(file, nParents + 1);
+                    getContent(file, nParents + 1, filtro);
                 }
             } else {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
@@ -65,3 +67,14 @@ public class EJ3_A2_UD1_XoanAraujoGandara {
         }
     }
 }
+
+class Filtro implements FilenameFilter{
+    String str;
+    public Filtro(String extension){
+        this.str = extension;
+    }
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.contains(str);
+    }
+} 
