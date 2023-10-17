244fdd31c817ef2b388138884ec93eff4f0f4ac4
xoanaraujo <115078254+xoanaraujo@users.noreply.github.com>
2023-10-17 09:52:40 +0200

    ...

--- UD2/A1UD2_XoanAraujoGandara/src/ej1_a1ud2_xoanaraujogandara/Ej1_a1ud2_XoanAraujoGandara.java
+++ UD2/A1UD2_XoanAraujoGandara/src/ej1_a1ud2_xoanaraujogandara/Ej1_a1ud2_XoanAraujoGandara.java
@@ -1,9 +1,6 @@
 package ej1_a1ud2_xoanaraujogandara;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -35,11 +32,12 @@ public class Ej1_a1ud2_XoanAraujoGandara {
         System.out.println("Ignora comentarios: " + dbf.isIgnoringComments());
         dbf.setIgnoringElementContentWhitespace(true);
         System.out.println("======================================");
+
         // 4
         Document doc;
         try {
             DocumentBuilder docBuilder = dbf.newDocumentBuilder();
-            doc = docBuilder.parse(new File( ACTORES_XML));
+            doc = docBuilder.parse(new File(ACTORES_XML));
 
 
         } catch (ParserConfigurationException | IOException | SAXException e) {
@@ -48,12 +46,14 @@ public class Ej1_a1ud2_XoanAraujoGandara {
         System.out.println(doc.getXmlEncoding() + " encoding");
 
         System.out.println("======================================");
+/*
         // 6
         Node root = doc.getDocumentElement();
         System.out.println("Elemento raíz: " + root.getNodeName());
         System.out.println("Valor elemento raíz: " + root.getNodeValue());
         System.out.println("Numero de hijos: " + root.getChildNodes().getLength());
         System.out.println("======================================");
+
         // 7
         NodeList hijos = root.getChildNodes();
         System.out.println(root.getTextContent());
@@ -61,28 +61,72 @@ public class Ej1_a1ud2_XoanAraujoGandara {
             System.out.println(hijos.item(i).getTextContent());
         }
         System.out.println("======================================");
+
         // 8
         Node nodo = root.getFirstChild().getNextSibling();
-        System.out.println("Nobre del nodo:" +nodo.getNodeName()+ " Valor del nodo:" + nodo.getNodeValue());
+        System.out.println("Nobre del nodo: " + nodo.getNodeName() + " Valor del nodo: " + nodo.getNodeValue());
         System.out.println("Se accede al hermano del primer hijo del nodo raiz");
         System.out.println("======================================");
+
         // 9
         Node elisa = root.getFirstChild();
         NodeList nodos = elisa.getChildNodes();
-        System.out.println("<" + elisa.getAttributes().item(0).getNodeName() +">:" + elisa.getAttributes().item(0).getNodeValue());
+        System.out.println("<" + elisa.getAttributes().item(0).getNodeName() + ">:" + elisa.getAttributes().item(0).getNodeValue());
         for (int i = 0; i < nodos.getLength(); i++) {
             System.out.println("<" + nodos.item(i).getNodeName() + ">" + nodos.item(i).getTextContent());
         }
         System.out.println("======================================");
+
         // 10
         Node deep = elisa.getNextSibling();
         nodos = deep.getChildNodes();
-        System.out.println("<" + deep.getAttributes().item(0).getNodeName() +">:" + deep.getAttributes().item(0).getNodeValue());
+        System.out.println("<" + deep.getAttributes().item(0).getNodeName() + ">:" + deep.getAttributes().item(0).getNodeValue());
         for (int i = 0; i < nodos.getLength(); i++) {
             System.out.println("<" + nodos.item(i).getNodeName() + ">" + nodos.item(i).getTextContent());
         }
         System.out.println("======================================");
+
         // 11
-        
+        Node ford = deep.getNextSibling();
+        nodos = ford.getChildNodes();
+        System.out.println("<" + ford.getAttributes().item(0).getNodeName() + ">:" + ford.getAttributes().item(0).getNodeValue());
+        for (int i = 0; i < nodos.getLength(); i++) {
+            System.out.println("<" + nodos.item(i).getNodeName() + ">" + nodos.item(i).getTextContent());
+        }
+        System.out.println("======================================");
+
+        // 12*/
+        printXML(doc.getDocumentElement(), 0);
+    }
+
+    private static void readNode(Node node, int nChild, int height, boolean writeOk){
+        if (writeOk){
+        System.out.println( "\t".repeat(height) + "-" + node.getNodeName() + ": " + (node.getNodeValue() == null ? "" : node.getNodeValue()));
+        NamedNodeMap attributes = node.getAttributes();
+        if (attributes != null)
+            for (int i = 0; i < attributes.getLength(); i++) {
+                System.out.println("\t".repeat(height) + "\t-" + attributes.item(i).getNodeName() + ": " + attributes.item(i).getNodeValue());
+            }
+        }
+        if(node.hasChildNodes() && node.getChildNodes().item(nChild) != null){
+            readNode(node.getChildNodes().item(nChild), 0, height + 1, true);
+        } else{
+            readNode(node.getParentNode().getParentNode(), nChild + 1, height - 1, false);
+        }
+    }
+
+    private static void printXML(Node node, int height){
+        NamedNodeMap attributes;
+        StringBuilder atrBuilder = new StringBuilder();
+        if (node.hasAttributes()){
+            attributes = node.getAttributes();
+            for (int i = 0; i < attributes.getLength(); i++) {
+                atrBuilder.append(" ").append(attributes.item(i).getNodeName()).append("=\"").append(node.getNodeValue()).append("\"");
+            }
+        }
+        if (node.getNodeValue() == null)
+            System.out.println( "\t".repeat(height) + "<" + node.getNodeName() + atrBuilder + ">");
+        else
+            System.out.println( "\t".repeat(height) + "<" + node.getNodeName() + ">" +node.getNodeValue()+ "</" + node.getNodeName() + ">");
     }
 }
